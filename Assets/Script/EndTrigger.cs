436c4ab1c99a20c679f13a044a27efa0984440c0
SittikronNon <sittikron.lamulsab@gmail.com>
2022-10-10 23:43:12 +0700

    testing collision and new scene

---
+++ Assets/Script/EndTrigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+   
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+
+       
+    }
+}
